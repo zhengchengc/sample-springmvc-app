0cb6076ce86c8215860be17b2d207f47a7e70eaa
olichid <zhengchengc@student.unimelb.edu.au>
2019-01-05 00:02:50 +1100

    Injecting literal value

--- samplespringmvcapp/src/main/java/com/olichid/springdemo/CricketCoach.java
+++ samplespringmvcapp/src/main/java/com/olichid/springdemo/CricketCoach.java
@@ -4,6 +4,11 @@ public class CricketCoach implements Coach{
 
     private FortuneService fortuneService;
 
+    // add new fields for emailaddress and team
+    private String emailAddress;
+    private String team;
+
+
     // create a no-arg constructor
     public CricketCoach() {
         System.out.println("CricketCoach: inside no-arg constructor");
@@ -13,6 +18,24 @@ public class CricketCoach implements Coach{
     public void setFortuneService(FortuneService fortuneService) {
         System.out.println("CricketCoach: inside setter method");
         this.fortuneService = fortuneService;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
     }
 
     public String getDailyWorkout() {
