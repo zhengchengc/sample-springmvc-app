8b3af17f870aab4475b8b6a87de8b7eb31c5a374
olichid <zhengchengc@student.unimelb.edu.au>
2019-03-01 00:05:01 +1100

    add data to the spring model

--- spring-mvc-demo/src/com/olichid/springmvc/demo/HelloWorldController.java
+++ spring-mvc-demo/src/com/olichid/springmvc/demo/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.olichid.springmvc.demo;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -15,6 +18,24 @@ public class HelloWorldController {
     // need a controller method to process the HTML form
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    // new a controller method to read form data and add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        theName = "Yo! " + theName;
+        // add message to the model
+        model.addAttribute("message", theName);
+
         return "helloworld";
     }
 }
--- spring-mvc-demo/web/WEB-INF/view/helloworld.jsp
+++ spring-mvc-demo/web/WEB-INF/view/helloworld.jsp
@@ -6,7 +6,7 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
+<br>
 <head>
     <title>Title</title>
 </head>
@@ -15,5 +15,10 @@ Hello World of Spring!
 <br></br>
 
 Student name: ${param.studentName}
+
+<br></br>
+
+The message: ${message}
+
 </body>
 </html>
